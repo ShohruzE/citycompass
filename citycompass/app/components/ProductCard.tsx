ce8aaf14835add8143e07cdbbca5a26aa94b96e9
NathanRVazquez <nathan.vazquez58@myhunter.cuny.edu>
2025-09-16 02:29:10 -0400

    initialized project frontend and updated readme with local deployment instructions

---
+++ citycompass/app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AddToCart from "./AddToCard";
+
+const ProductCard = () => {
+    return (
+        <div> Product Card 
+            <AddToCart />
+        </div>
+    )
+}
+export default ProductCard
